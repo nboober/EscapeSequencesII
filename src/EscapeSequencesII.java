2f96674cfac828d8d43ab86c562b0d9f1d99d137
nboober <nboober@terpmail.umd.edu>
2019-06-12 14:00:47 -0400

    Initial Commit

---
+++ src/EscapeSequencesII.java
@@ -0,0 +1,9 @@
+public class EscapeSequencesII {
+    public static void main(String[] args){
+        System.out.println("Cost\t\tQuantity\t\tTotal");
+        System.out.println("$1,000.00\t\t4\t\t$4,000.00");
+        System.out.println("$   50.00\t\t8\t\t  $400.00");
+        System.out.println("=================================");
+        System.out.println("Total:\t\t\t12\t\t\t\b$4,400.00");
+    }
+}
